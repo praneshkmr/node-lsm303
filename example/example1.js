09ec92a9cb529aa3ce618635f424f643eab29fd7
Pranesh Kumar <praneshkmr@gmail.com>
2014-04-24 00:34:04 +0000

    Added basic Accelerometer and Magnetometer Functionality

---
+++ example/example1.js
@@ -0,0 +1,33 @@
+var lsm303 = require('../index');
+
+var ls  = new lsm303();
+
+var accel = ls.accelerometer();
+var mag = ls.magnetometer();
+
+accel.readAxes(function(err,axes){
+    if(err){
+        console.log("Error reading Accelerometer Axes : " + err);
+    }
+    if (axes) {
+        console.log(axes);
+    }
+});
+
+mag.readAxes(function(err,axes){
+    if(err){
+        console.log("Error reading Magnetometer Axes : " + err);
+    }
+    if (axes) {
+        console.log(axes);
+    }
+});
+
+mag.readTemp(function(err,temp){
+    if(err){
+        console.log("Error reading Temperature : " + err);
+    }
+    if (temp) {
+        console.log(temp);
+    }
+});
